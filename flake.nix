d62afec77c946ff271bc349b19e0c84ca80f58f9
losnappas <hanu6@hotmail.com>
2025-11-17 11:37:16 +0200

    flake: remove devenv

--- flake.nix
+++ flake.nix
@@ -4,21 +4,13 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devenv.url = "github:cachix/devenv";
     systems.url = "github:nix-systems/default";
-    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs =
     inputs@{ flake-parts, self, ... }:
     flake-parts.lib.mkFlake { inherit inputs self; } {
       imports = [
-        inputs.devenv.flakeModule
       ];
       systems = import inputs.systems;
       perSystem =
@@ -36,20 +28,16 @@
           # Per-system attributes can be defined here. The self' and inputs'
           # module parameters provide easy access to attributes of the same
           # system.
-
-          devenv.shells.default = {
-            # https://devenv.sh/reference/options/
-            languages.nix.enable = true;
-            languages.shell.enable = true;
-
+          devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               unixtools.script
+              nil
+              bash-language-server
+              shellcheck-minimal
             ];
-
-            # enterShell = '''';
           };
 
-          formatter = pkgs.nixfmt-rfc-style;
+          formatter = pkgs.nixfmt-tree;
 
           packages.default = pkgs.kakouneUtils.buildKakounePluginFrom2Nix {
             pname = "kak-text-file-terminal";
